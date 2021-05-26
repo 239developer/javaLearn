9fca2ab503631bf1fe6610d58906a5cdf340a93a
@the_forg <factoriogmer@gmail.com>
2021-05-26 16:36:20 +0300

    building reactive rest service

---
+++ reactiverestservice/src/main/java/com/example/reactiverestservice/ReactiverestserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactiverestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiverestserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiverestserviceApplication.class, args);
+	}
+
+}
---
+++ reactiverestservice/src/test/java/com/example/reactiverestservice/ReactiverestserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactiverestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiverestserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
